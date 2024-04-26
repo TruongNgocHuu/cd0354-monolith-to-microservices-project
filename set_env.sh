72cf964e0c7a0443784e3e3397ed438f7c571b48
Ngoc Huu <huutruong960@gmail.com>
2024-04-26 15:36:17 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=huutocdai
+export POSTGRES_PASSWORD=0828468260
+export POSTGRES_HOST=project3.cknorfhs8ijn.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::project3-258669172645-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
