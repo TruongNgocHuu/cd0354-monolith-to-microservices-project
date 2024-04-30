cf5947ae3d91163d948e376ef0eaa8a601e6544e
Ngoc Huu <huutruong960@gmail.com>
2024-05-01 00:55:59 +0700

    add all

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1f1c34ff6cb0461ab793f935e321481-229645835.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
